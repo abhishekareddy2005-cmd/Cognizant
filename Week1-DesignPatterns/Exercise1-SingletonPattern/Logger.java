404cf9098125b34f7f6eb14939b25918dd0fb51a
abhishekareddy2005-cmd <abhishekareddy2005@gmail.com>
2026-07-16 16:40:30 +0530

    .

--- Week1-DesignPatterns/Exercise1-SingletonPattern/Logger.java
+++ Week1-DesignPatterns/Exercise1-SingletonPattern/Logger.java
@@ -1,16 +1,12 @@
 public class Logger {
-    // 1. Private static instance
     private static Logger instance;
 
-    // 2. Private constructor prevents instantiation from other classes
     private Logger() {
-        System.out.println("Logger Initialized.");
+        System.out.println("Logger Initialised.");
     }
 
-    // 3. Public static method to provide global access
     public static Logger getInstance() {
         if (instance == null) {
-            // Using synchronized to ensure thread safety
             synchronized (Logger.class) {
                 if (instance == null) {
                     instance = new Logger();
@@ -21,6 +17,6 @@ public class Logger {
     }
 
     public void log(String message) {
-        System.out.println("Log message: " + message);
+        System.out.println("log message: " + message);
     }
 }
