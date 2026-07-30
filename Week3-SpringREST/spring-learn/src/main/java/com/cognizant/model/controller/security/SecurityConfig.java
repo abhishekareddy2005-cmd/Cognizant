3cce971f1c98ef546ebd39d8b4f06421e2e798dc
abhishekareddy2005-cmd <abhishekareddy2005@gmail.com>
2026-07-30 16:36:45 +0530

    Create SecurityConfig.java

---
+++ Week3-SpringREST/spring-learn/src/main/java/com/cognizant/model/controller/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.cognizant.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // in memory users
+        auth.inMemoryAuthentication()
+            .withUser("admin").password("{noop}admin123").roles("ADMIN")
+            .and()
+            .withUser("user").password("{noop}user123").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/authenticate").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
