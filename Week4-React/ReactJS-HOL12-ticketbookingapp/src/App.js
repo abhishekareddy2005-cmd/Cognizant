458fff17af084970312939b2e6d2a12ac343c414
abhishekareddy2005-cmd <abhishekareddy2005@gmail.com>
2026-07-30 16:54:08 +0530

    Create App.js

---
+++ Week4-React/ReactJS-HOL12-ticketbookingapp/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import GuestPage from './GuestPage';
+import UserPage from './UserPage';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    };
+  }
+
+  handleLogin = () => {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogout = () => {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ticket Booking App</h1>
+
+        {this.state.isLoggedIn ? (
+          <button onClick={this.handleLogout}>Logout</button>
+        ) : (
+          <button onClick={this.handleLogin}>Login</button>
+        )}
+
+        {this.state.isLoggedIn ? <UserPage /> : <GuestPage />}
+      </div>
+    );
+  }
+}
+
+export default App;
